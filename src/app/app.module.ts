b0facdb914305165127b975ce8516ea014370553
0nk3 <0nk3.0nk3@gmail.com>
2020-09-30 11:03:55 +0200

    generate god componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DogComponentComponent } from './dog-component/dog-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogComponentComponent
   ],
   imports: [
     BrowserModule,
--- src/app/dog-service.service.ts
+++ src/app/dog-service.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DogServiceService {
+  constructor(private httpClient: HttpClient) {}
+  randomDog(): Observable<Dog> {
+    return this.httpClient.get<Dog>(`https://dog.ceo/api/breeds/image/random`);
+  }
+}
 
-  constructor() { }
+export interface Dog {
+  message: string;
+  status: string;
 }
